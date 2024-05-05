04b7c941aff49413612df43a1de3587dd2c65f1f
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-05-05 11:50:08 +0000

    Declare constants for DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Declare constants for DOM elements
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+
