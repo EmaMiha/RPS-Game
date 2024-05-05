e0efe9273498c3f955dfdcf5a45185aba93fd684
Milena Mihajilovic <ema.mihajilovic@gmail.com>
2024-05-05 12:07:59 +0000

    Creating function playGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,26 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 
 
+function playGame (playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() *3)];
+    let result = "";
+
+    if (playerChoice===computerChoice) {
+        result = "TIE!";
+    }
+    else {
+        switch(playerChoice) {
+            case "rock" :
+             result = (computerChoice ==="scissors") ? "YOU WIN!" : "YOU LOSE!"
+             break;
+             case "paper" :
+             result = (computerChoice ==="rock") ? "YOU WIN!" : "YOU LOSE!"
+             break;
+             case "scissors" :
+             result = (computerChoice ==="paper") ? "YOU WIN!" : "YOU LOSE!"
+             break;
+        }
+    }
+
+    
+}
